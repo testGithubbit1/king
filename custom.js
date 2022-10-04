ea3bf9f0783d5f0bd1a4c50336e2b2777ff57fc9
testGithubbit1 <106900574+testGithubbit1@users.noreply.github.com>
2022-10-04 14:07:31 -0600

    ghhgfh

---
+++ custom.js
@@ -0,0 +1,16 @@
+var typed = new Typed("#tyoing", {
+    strings: ["web desing", "web developer",],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+})
+
+
+var typed = new Typed("#tyoing2", {
+    strings: ["web desing", "web developer",],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+})
+
+
